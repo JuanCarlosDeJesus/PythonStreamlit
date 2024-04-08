9fd5de7ca071a71a6034c71e1c0435565977210b
JuanCarlos.DeJesus <45457547+JuanCarlosDeJesus@users.noreply.github.com>
2024-04-08 16:35:31 +0800

    3rd push

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ import streamlit as st
 st.title("This is st.title")
 st.subheader("This is st.subheader")
 st.header("This is st.header")
-st.text("This is st.textso")
+st.text("This is st.text - use it in place of paragraphs")
+# markdown 
+st.markdown("[MarkdownGuide.org](https://www.markdownguide.org)")
