8382b5c0d6c7cd1ec18f83d5e9e1b90871e86cbf
JuanCarlos.DeJesus <45457547+JuanCarlosDeJesus@users.noreply.github.com>
2024-04-19 16:54:13 +0800

    chap 6 and 7

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import streamlit as st
 import pandas as pd
 
+# remove "hamburger" - options on top right and others
+st.text("Streamlit cleanup - hamburger and deploy, right click on hamburger and click inspect")
+st.text("Then use .markdown, in class names use '.' for blank space")
+st.markdown("""
+<style>
+.st-emotion-cache-6q9sum.ef3psqc4
+{
+    visibility: hidden;
+}
+.st-emotion-cache-1wbqy5l.e17vllj40
+{
+    visibility: hidden;
+}
+</style>
+""", unsafe_allow_html=True)
+
 tbl=pd.DataFrame({"Column 1": [1,2,3,4,5,6,7], "Column 2": [11,12,13,14,15,16,17]})
 
 st.title("This is st.title")
@@ -28,3 +44,10 @@ st.text("st.table is static")
 st.table(tbl)
 st.text("st.dataframe can be manipulated")
 st.dataframe(tbl)
+
+# display media files
+st.text("Display media files with .audio, .image, .video")
+st.image("images.jpg", caption="This is my image", width=680)
+st.audio("KavinskyNightcall.mp3")
+st.video("KaliLinuxInstall.mp4")
+
