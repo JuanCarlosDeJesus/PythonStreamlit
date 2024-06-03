d1207be0b8b43210c37c23fc2e6b5566d52c3c2d
Juan Carlos De Jesus <carl.d3j3sus@gmail.com>
2024-06-03 16:50:47 +0800

    Webpage setup

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# find more emojis here: webfx.com/tools/emoji-cheat-sheet
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+# ------ HEADER SECTION -------
+st.subheader("Hi, I am Carl :wave:")
+st.title("A Python Developer from the US")
+st.write("I am using python and VBA to more efficient in data analysis")
+st.write("[Learn More >](https://pythonandvba.com)")
