362fdd98984297e274db834f8d5812806db350e7
JuanCarlos.DeJesus <45457547+JuanCarlosDeJesus@users.noreply.github.com>
2024-06-13 17:00:08 +0800

    added contact form

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from PIL import Image
 import requests
 import streamlit as st
 from streamlit_lottie import st_lottie
@@ -13,6 +14,8 @@ def load_lottieurl(url):
 
 # ---- LOAD ASSETS ----
 lottie_coding = load_lottieurl("https://lottie.host/41b08c2e-66e7-49d6-bfae-05ae89860138/ZTdTguFYDQ.json")
+img_contact_form = Image.open("images/yt_contact_form.png")
+img_lottie_animation = Image.open("images/yt_lottie_animation.png")
 
 # ---- HEADER SECTION ----
 with st.container():
@@ -48,7 +51,7 @@ with st.container():
         st.write("##")
         image_column, text_column = st.columns((1, 2))
         with image_column:
-            st.write("")
+            st.image(img_lottie_animation)
         with text_column:
             st.subheader("Integrate Lottie animation inside your Streamlit App")
             st.write(
@@ -59,3 +62,38 @@ with st.container():
                     """
                      )
             st.markdown("[Watch video ... ](https://youtu.be/TXSOitgoINE)")
+    with st.container():
+        image_column, text_column = st.columns((1, 2))
+        with image_column:
+            st.image(img_contact_form)
+        with text_column:
+            st.subheader("How To Add A Contact Form To Your Streamlit App")
+            st.write(
+                    """
+                    Want to add a contact form to your Streamlit website?
+                    In this video, I'm going to show you how to implement a contact form in your Strealit app using the free service 'Form Sumbit'.
+                    """
+                     )
+            st.markdown("[Watch video ... ](https://youtu.be/FOULV9Xij_8)")
+    
+# ---- CONTACT ----
+with st.container():
+    st.write("---")
+    st.header("Get In Touch With Me!")
+    st.write("##")
+
+    # Documentation: https://formsubmit.co/  !!! CHANGE EMAIL ADDRESS !!!
+    contact_form = """
+    <form action="https://formsubmit.co/carl.d3j3sus@gmail.com" method="POST">
+        <input type="hidden" name="_captcha" value="false">
+        <input type="text" name="name" placeholder="Your Name" required>
+        <input type="email" name="email" placeholder="Your Email" required>
+        <textarea name="message" placeholder="Your Message Here" required></textarea>
+        <button type="submit">Send</button>
+    </form> 
+    """
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.markdown(contact_form, unsafe_allow_html=True)
+    with right_column:
+        st.empty()
