4e222fba41452ee17b023aced96eca0921a50e99
JuanCarlos.DeJesus <45457547+JuanCarlosDeJesus@users.noreply.github.com>
2024-06-23 21:41:38 +0800

    ExcelDashboard

--- ContactForm/contact.py
+++ ContactForm/contact.py
@@ -12,3 +12,10 @@ contact_form = """
 </form> 
 """
 st.markdown(contact_form, unsafe_allow_html=True)
+
+# Use local CSS file
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+local_css("style/style.css")
