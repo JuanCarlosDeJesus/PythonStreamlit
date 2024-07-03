4cf04a767bff46a14cf201f006ca62a7cfe09a7b
JuanCarlos.DeJesus <45457547+JuanCarlosDeJesus@users.noreply.github.com>
2024-07-03 17:08:56 +0800

    Added barchart

--- ExcelDashboard/excel.py
+++ ExcelDashboard/excel.py
@@ -65,6 +65,19 @@ with right_column:
 
 st.markdown("---")
 
+# st.dataframe(df_selection) - pandas
+# SALES BY PRODUCT LINE[BAR CHART]
+sales_by_product_line = (
+    df_selection.groupby(by=["Product line"]).sum()[["Total"]].sort_values(by="Total")
+)
+fig_product_sales = px.bar(
+    sales_by_product_line,
+    x="Total",
+    y=sales_by_product_line.index,
+    orientation="h",
+    title="<b>Sales by Product Line</b>",
+    color_discrete_sequence=["#0083B8"] * len(sales_by_product_line),
+    template="plotly_white",
+)
 
-
-# st.dataframe(df_selection)
+st.plotly_chart(fig_product_sales)
