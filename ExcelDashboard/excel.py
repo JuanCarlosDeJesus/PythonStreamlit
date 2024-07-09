dd2452ceb71d574da1e934d18b91967f69d3670e
JuanCarlos.DeJesus <45457547+JuanCarlosDeJesus@users.noreply.github.com>
2024-07-09 17:24:29 +0800

    Finish Excel

--- ExcelDashboard/excel.py
+++ ExcelDashboard/excel.py
@@ -7,15 +7,23 @@ st.set_page_config(page_title="Sales Dashboard",
                    page_icon=":bar_chart:",
                    layout="wide")  # set page to occupy the entire screen
 
-df = pd.read_excel(
-    io='supermarkt_sales.xlsx',
-    engine='openpyxl',
-    sheet_name='Sales',
-    skiprows=3,
-    usecols='B:R',
-    nrows=1000,
-)
+# ---- READ EXCEL ----
+@st.cache_data
+def get_data_from_excel():
+    df = pd.read_excel(
+        io='supermarkt_sales.xlsx',
+        engine='openpyxl',
+        sheet_name='Sales',
+        skiprows=3,
+        usecols='B:R',
+        nrows=1000,
+    )
 
+    # Add 'hour' column to dataframe
+    df["hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
+    return df
+
+df = get_data_from_excel()
 
 # ---- SIDEBAR ----
 st.sidebar.header("Please Filter Here:")
@@ -78,4 +86,39 @@ fig_product_sales = px.bar(
     template="plotly_white",
 )
 
-st.plotly_chart(fig_product_sales)
+fig_product_sales.update_layout(
+    plot_bgcolor="rgba(0,0,0,0)",
+    xaxis=(dict(showgrid=False))
+)
+
+
+# SALES BY HOUR [BAR CHART]
+sales_by_hour = df_selection.groupby(by=["hour"])[["Total"]].sum()
+fig_hourly_sales = px.bar(
+    sales_by_hour,
+    x=sales_by_hour.index,
+    y="Total",
+    title="<b>Sales by the Hour</b>",
+    color_discrete_sequence=["#0083B8"] * len(sales_by_hour),
+    template="plotly_white",
+)
+fig_hourly_sales.update_layout(
+    xaxis=dict(tickmode="linear"),
+    plot_bgcolor="rgba(0,0,0,0)",
+    yaxis=(dict(showgrid=False)),
+)
+
+left_column, right_column = st.columns(2)
+left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
+right_column.plotly_chart(fig_product_sales, use_container_width=True)
+
+# ---- HIDE STREAMLIT STYLE ----
+hide_st_style = """
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                header {visibility: hidden;}
+                </style>
+"""
+
+st.markdown(hide_st_style, unsafe_allow_html=True)
