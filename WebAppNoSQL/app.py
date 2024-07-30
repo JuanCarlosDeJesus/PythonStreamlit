5c08134120719ac0b691b7f2b0d79c24b8e0479a
JuanCarlos.DeJesus <45457547+JuanCarlosDeJesus@users.noreply.github.com>
2024-07-30 23:05:14 +0800

    Sankey update

--- WebAppNoSQL/app.py
+++ WebAppNoSQL/app.py
@@ -48,3 +48,29 @@ with st.form("entry_form", clear_on_submit=True):
         st.write(f"incomes: {incomes}")
         st.write(f"expenses: {expenses}")
         st.success("Data Saved!")
+
+
+# --- PLOT PERIODS ---
+st.header("Data Visualization")
+with st.form("saved_periods"):
+    # TODO: Get periods from database
+    period = st.selectbox("Select Period:", ["2024_March"])
+    submitted = st.form_submit_button("Plot Period")
+    if submitted:
+        # TODO: Get data from database
+        comment = "Some comment"
+        incomes = {'Salary': 1500, 'Blog': 50, 'Other Income': 10}
+        expenses = {'Rent': 600, 'Utilities': 200, 'Groceries': 300, 'Car': 100, 'Other Expenses': 50, 'Savings': 10}
+
+        # Create metrics
+        total_income = sum(incomes.values())
+        total_expense = sum(expenses.values())
+        remaining_budget = total_income - total_expense
+        col1, col2, col3 = st.columns(3)
+        col1.metric("Total Income", f"{total_income} {currency}")
+        col2.metric("Total Expense", f"{total_expense} {currency}")
+        col3.metric("Remaining Budget", f"{remaining_budget} {currency}")
+        st.text(f"Comment: {comment}")
+
+        # Create sankey chart
+        label = list(incomes.keys()) + ["Total Income"]
