86d28059525b647857c501d2d724b9375ada0e54
JuanCarlos.DeJesus <45457547+JuanCarlosDeJesus@users.noreply.github.com>
2024-08-07 15:33:09 +0800

    Resume 2nd commit

--- Resume/app.py
+++ Resume/app.py
@@ -35,4 +35,62 @@ PROJECTS = {
 
 st.set_page_config(page_title=PAGE_TITTLE, page_icon=PAGE_ICON)
 
-st.title("Hello There!")
+# --- LOAD CSS, PDF & PROFILE PIC ---
+with open(css_file) as f:
+    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+with open(resume_file, "rb") as pdf_file:
+    PDFbyte = pdf_file.read()
+profile_pic = Image.open(profile_pic)
+
+# --- HERO SECTION ---
+col1, col2 = st.columns(2, gap="small")
+with col1:
+    st.image(profile_pic, width=230)
+
+with col2:
+    st.title(NAME)
+    st.write(DESCRIPTION)
+    st.download_button(
+        label=" 📜 Download CV",
+        data=PDFbyte,
+        file_name=resume_file.name,
+        mime="application/octet-stream",        
+    )
+    st.write("📩", EMAIL)
+
+# --- SOCIAL LINKS ---
+st.write("#")
+cols = st.columns(len(SOCIAL_MEDIA))
+for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
+    cols[index].write(f"[{platform}]({link})")
+
+# --- EXPERIENCE & QUALIFICATION ---
+st.write("#")
+st.subheader("Experience & Qualifications")
+st.write(
+    """
+- ✔️  15 Years experiene extracting actionable insights from data
+- ✔️  Strong hands on experience and knowledge in Python and Excel
+- ✔️  Good understanding so statistical principles and their respective applications
+- ✔️  Excellnt team-player and displaying strong sense of initiative on tasks 
+"""
+)
+
+# --- SKILLS ---
+st.write("#")
+st.subheader("Hard Skills")
+st.write(
+    """
+- 🧑‍💻 Programming: Python (Streamlit, Flet, Pandas), SQL, VBA
+- 📊 Data Visualization: Plotly, Ms Excel, PowerBi
+- 🧾 Modeling: Logistic regression, Linear regression, Decision trees
+"""
+)
+
+# --- WORK HISTORY ---
+st.write("#")
+st.subheader("Work History")
+st.write("---")
+
+# --- JOB 1 ---
+st.write("")
