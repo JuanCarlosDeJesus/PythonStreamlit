3e88a1bdcc6efccf9870293fc3444700d99fceb6
JuanCarlos.DeJesus <45457547+JuanCarlosDeJesus@users.noreply.github.com>
2024-08-10 22:17:31 +0800

    Payment complete

--- OnlinePayment/payment_app.py
+++ OnlinePayment/payment_app.py
@@ -13,6 +13,7 @@ CSS_FILE = STYLES_DIR / "main.css"
 # --- GENERAL SETTINGS ---
 STRIPE_CHECKOUT = "https://buy.stripe.com/test_aEU6rm20KgGj86YaEE"
 CONTACT_EMAIL = "YOUREMAIL@EMAIL.COM"
+DEMO_VIDEO = "https://youtu.be/PmJ9rkKGqrI"
 PRODUCT_NAME = "Excel Add-In: MyToolBelt"
 PRODUCT_TAGLINE = "Ready to Become an Office Superhero? 🚀"
 PRODUCT_DESCRIPTION = """
@@ -28,6 +29,11 @@ wont find anywhere else:
 **This is your new superpower; why fo to work without it?**
 """
 
+def load_css_file(css_file_path):
+    with open(css_file_path) as f:
+        return st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
+
+
 # --- PAGE CONFIG ---
 st.set_page_config(
     page_title=PRODUCT_NAME,
@@ -35,6 +41,7 @@ st.set_page_config(
     layout="centered",
     initial_sidebar_state="collapsed",
 )
+load_css_file(CSS_FILE)
 
 # --- MAIN SECTION ---
 st.header(PRODUCT_NAME)
@@ -50,3 +57,65 @@ with left_col:
 with right_col:
     product_image = PIL.Image.open(ASSETS_DIR / "toolbelt.png")
     st.image(product_image, width=450)
+
+# --- FEATURES ---
+st.write("")
+st.write("---")
+st.subheader(":rocket: Features")
+features = {
+    "Feature_1.png": [
+        "Run Python Files From Excel",
+        "After locating your Python interpreter, you can execute Python files directly from Excel. In the Pro Version, you can also add several Python interpreter paths. This is helpful when you need to execute your Python code from different virtual environments.",
+    ],
+    "Feature_2.png": [
+        "Create Pandas Dataframes",
+        "Generate Python files with a click of a button. Select the cell range you want to transform, and the add-in creates the Python code to rad in the Excel data as a pandas dataframe, Instead of messing around with all of the available options in the pandas 'read_excel' method, the add-in does it for you"
+    ],
+    "Feature_3.png": [
+        "Create Jupyter Notebooks",
+        "Have you ever wanted to do some quick analysis of your Exel data in a Jupyter notebook? MyToolBelt can convert an Excel cell range into a Jupyter Notebook. Just select the cell range, and the add-in will create a new Jupyter Notebook in the workbook's directory, Inside the Jupyter Notebook, you will find your 'ready-to-use' dataframe based on your selection. This feature is a real time saver!",
+    ],
+}
+for image, description in features.items():
+    image = PIL.Image.open(ASSETS_DIR / image)
+    st.write("")
+    left_col, right_col = st.columns(2)
+    left_col.image(image, use_column_width=True)
+    right_col.write(f"**{description[0]}**")
+    right_col.write(description[1])
+
+# --- DEMO VIDEO ---
+st.write("")
+st.write("---")
+st.subheader(":tv: Demo")
+st.video(DEMO_VIDEO, format="video/mp4", start_time=0)
+
+# --- FAQ ---
+st.write("")
+st.write("---")
+st.subheader(":raising_hand: FAQ")
+faq = {
+    "Question 1": "Some text goes here to answer question 1",
+    "Question 2": "Some text goes here to answer question 2",
+    "Question 3": "Some text goes here to answer question 3",
+    "Question 4": "Some text goes here to answer question 4",
+    "Question 5": "Some text goes here to answer question 5",
+}
+for question, answer in faq.items():
+    with st.expander(question):
+        st.write(answer)
+
+# --- CONTACT FORM ---
+st.write("")
+st.write("---")
+st.subheader(":mailbox: Have a Question? Ask Away!")
+contact_form = f"""
+<form action="https://formsubmit.co/{CONTACT_EMAIL}" method="POST">
+    <input type="hidden" name="_captcha" value="false">
+    <input type="text" name="name" placeholder="Your Name" required>
+    <input type="email" name="email" placeholder="Yout Email" required>
+    <textarea name="message" placeholder="Your message here"></textarea>
+    <button type="submit" class="button">Send ✉️</button>
+</form>
+"""
+st.markdown(contact_form, unsafe_allow_html=True)
