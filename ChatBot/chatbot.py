7c7a07261af8873a2a4acf937e21f83ab61210b9
JuanCarlos.DeJesus <45457547+JuanCarlosDeJesus@users.noreply.github.com>
2024-08-14 16:33:52 +0800

    Chatbot clone initial commit

---
+++ ChatBot/chatbot.py
@@ -0,0 +1,43 @@
+import openai # pip install openai
+import streamlit as st # pip install streamlit --upgrade
+
+
+st.title("ChatGPT Clone")
+
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+# Initialize chat history
+if "message" not in st.session_state:
+    st.session_state.message = []
+
+# Display chat messages from history in app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "| ")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
